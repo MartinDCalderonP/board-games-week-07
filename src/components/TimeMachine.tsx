471ff7544d205c72c41d94d69c59180e236756b2
Martín Calderón <martincalderon92@outlook.com>
2021-10-27 23:18:13 -0300

    :recycle: Change function name to more readability.

--- src/components/TimeMachine.tsx
+++ src/components/TimeMachine.tsx
@@ -7,11 +7,11 @@ const squares = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 export default function TimeMachine() {
 	const [currentSquare, setCurrentSquare] = useState(0);
-	const [previous, getPreviousValue, record] = useTimeMachine(currentSquare);
+	const [previous, getValueOnPosition, record] = useTimeMachine(currentSquare);
 
 	const handleSquareClick = (index: number) => {
 		setCurrentSquare(index + 1);
-		console.log(record);
+		console.log(previous, record);
 	};
 
 	return (
--- src/hooks/useTimeMachine.tsx
+++ src/hooks/useTimeMachine.tsx
@@ -5,12 +5,12 @@ export default function useTimeMachine<T>(
 ): [T, (position: number) => T, T[]] {
 	const ref: any = useRef<T>();
 	const record = useRef<T[]>([]);
-	const getPreviousValue = (position: number) => record.current[position];
+	const getValueOnPosition = (position: number) => record.current[position];
 
 	useEffect(() => {
 		ref.current = value;
 		record.current.unshift(value);
 	}, [value]);
 
-	return [ref.current, getPreviousValue, record.current];
+	return [ref.current, getValueOnPosition, record.current];
 }
