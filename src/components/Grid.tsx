2bc9177a6ea21a1a6bc6c72a08c32aa900ea2cc6
Martín Calderón <martincalderon92@outlook.com>
2021-10-27 22:52:49 -0300

    :art: Create Grid component.

---
+++ src/components/Grid.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from '../styles/Grid.module.scss';
+
+export default function Grid() {
+	return (
+		<div className={styles.grid}>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+			<div className={styles.square}></div>
+		</div>
+	);
+}
