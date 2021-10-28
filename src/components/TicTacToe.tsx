c05f18264b876aabf1a2e8f9312d412d7d927cd0
Martín Calderón <martincalderon92@outlook.com>
2021-10-28 17:01:48 -0300

    :art: Create layout of the TicTacToe.

---
+++ src/components/TicTacToe.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from '../styles/TicTacToe.module.scss';
+import Button from './Button';
+
+const squares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+export default function TicTacToe() {
+	return (
+		<div className={styles.ticTacToe}>
+			<div className={styles.grid}>
+				{squares.map(() => (
+					<div className={styles.square} />
+				))}
+			</div>
+
+			<div className={styles.controls}>
+				<Button>Next</Button>
+				<Button>Resume</Button>
+				<Button>Previous</Button>
+			</div>
+		</div>
+	);
+}
