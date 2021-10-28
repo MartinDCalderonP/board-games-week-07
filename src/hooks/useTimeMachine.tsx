bad4206f7838ad6699e1b812edde877ccd9558fe
Martín Calderón <martincalderon92@outlook.com>
2021-10-27 23:10:27 -0300

    :recycle: Change variable name history to record.

--- src/hooks/useTimeMachine.tsx
+++ src/hooks/useTimeMachine.tsx
@@ -4,13 +4,13 @@ export default function useTimeMachine<T>(
 	value: T
 ): [T, (position: number) => T, T[]] {
 	const ref: any = useRef<T>();
-	const history = useRef<T[]>([]);
-	const getPreviousValue = (position: number) => history.current[position];
+	const record = useRef<T[]>([]);
+	const getPreviousValue = (position: number) => record.current[position];
 
 	useEffect(() => {
 		ref.current = value;
-		history.current.unshift(value);
+		record.current.unshift(value);
 	}, [value]);
 
-	return [ref.current, getPreviousValue, history.current];
+	return [ref.current, getPreviousValue, record.current];
 }
