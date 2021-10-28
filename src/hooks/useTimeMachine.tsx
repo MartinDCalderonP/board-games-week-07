357b075f1bb4f3cdc3466e8a870f206cbc2cd427
Martín Calderón <martincalderon92@outlook.com>
2021-10-28 15:23:56 -0300

    :fire: Delete unused function.

--- src/hooks/useTimeMachine.tsx
+++ src/hooks/useTimeMachine.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useRef } from 'react';
 
-export default function useTimeMachine<T>(
-	value: T
-): [T, (position: number) => T, T[]] {
+export default function useTimeMachine<T>(value: T): [T, T[]] {
 	const ref: any = useRef<T>();
 	const record = useRef<T[]>([]);
-	const getValueOnPosition = (position: number) => record.current[position];
 
 	useEffect(() => {
 		ref.current = value;
 		record.current.unshift(value);
 	}, [value]);
 
-	return [ref.current, getValueOnPosition, record.current];
+	return [ref.current, record.current];
 }
