3454ed19544c9207b2b7bc519090cff45d6c8f57
Martín Calderón <martincalderon92@outlook.com>
2021-10-27 22:55:39 -0300

    :fire: Delete unused useState.

--- src/components/TimeMachine.tsx
+++ src/components/TimeMachine.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from '../styles/TimeMachine.module.scss';
 import Grid from './Grid';
 import Controls from './Controls';
