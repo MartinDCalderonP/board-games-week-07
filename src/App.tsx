014b0d9c95ad13f19d0522c09e8634c1022c46d7
Martín Calderón <martincalderon92@outlook.com>
2021-10-28 16:49:39 -0300

    :sparkles: Add Button to change the game.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './styles/App.module.scss';
 import TimeMachine from './components/TimeMachine';
+import TicTacToe from './components/TicTacToe';
+import Button from './components/Button';
 
 function App() {
+	const [game, setGame] = useState('ticTacToe');
+
+	const handleChangeGameButtonClick = () => {
+		setGame(game === 'ticTacToe' ? 'timeMachine' : 'ticTacToe');
+	};
+
 	return (
-		<div>
-			<TimeMachine />
+		<div className={styles.app}>
+			<Button
+				className={styles.changeGameButton}
+				onClick={handleChangeGameButtonClick}
+			>
+				Change Game
+			</Button>
+
+			{game === 'timeMachine' ? <TimeMachine /> : <TicTacToe />}
 		</div>
 	);
 }
